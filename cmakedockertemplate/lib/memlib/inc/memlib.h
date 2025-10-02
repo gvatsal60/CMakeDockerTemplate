356cd5edcfe48582c4ac5fad68c60c8b7f31af2a
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2025-10-02 09:19:15 +0000

    Fixed SonarQube Issues

--- cmakedockertemplate/lib/memlib/inc/memlib.h
+++ cmakedockertemplate/lib/memlib/inc/memlib.h
@@ -7,12 +7,10 @@
 
 template <typename T> static void MemClean(const T *const arg) {
   assert(arg != nullptr);
-  std::unique_ptr<const T> argPtr(arg);
 }
 
 template <typename T> static void MemArrClean(const T *const arg) {
   assert(arg != nullptr);
-  std::unique_ptr<const T[]> argPtr(arg);
 }
 
 #endif
