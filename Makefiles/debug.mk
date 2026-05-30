9508217c1bf7f82df98d8159ff544c26ccab3e16
gvatsal60 <gvatsal60@users.noreply.github.com>
2026-05-30 09:07:29 +0530

    Fix Local Build

--- Makefiles/debug.mk
+++ Makefiles/debug.mk
@@ -6,6 +6,7 @@ DOCKER_FILE_PATH := dockerfiles/Dockerfile.debug
 
 DOCKER_BUILD_CMD := $(DOCKER_HOST) image build -t $(DOCKER_IMG_NAME) -f $(DOCKER_FILE_PATH) $(DOCKER_BUILD_CONTEXT)
 DOCKER_RUN_CMD := $(DOCKER_HOST) container run $(DOCKER_ARG) $(DOCKER_IMG_NAME)
+CONTAINER_SHELL := /bin/sh -lc
 
 # Define the default target
 .PHONY: all debug release test clean run
@@ -19,20 +20,20 @@ build_img:
 
 # Debug Build
 debug: build_img
-	@$(DOCKER_RUN_CMD) $(DEBUG_CMD)
+	$(DOCKER_RUN_CMD) $(CONTAINER_SHELL) "$(DEBUG_CMD)"
 
 # Release Build
 release: build_img
-	@$(DOCKER_RUN_CMD) $(RELEASE_CMD)
+	@$(DOCKER_RUN_CMD) $(CONTAINER_SHELL) "$(RELEASE_CMD)"
 
 # Test code
 test: debug
-	@$(DOCKER_RUN_CMD) $(TEST_CMD)
+	@$(DOCKER_RUN_CMD) $(CONTAINER_SHELL) "$(TEST_CMD)"
 
 # Run code
 run: debug
-	@$(DOCKER_RUN_CMD) $(RUN_CMD)
+	@$(DOCKER_RUN_CMD) $(CONTAINER_SHELL) "$(RUN_CMD)"
 
 # Clean
 clean: build_img
-	@$(DOCKER_RUN_CMD) $(CLEAN_CMD)
+	@$(DOCKER_RUN_CMD) $(CONTAINER_SHELL) "$(CLEAN_CMD)"
